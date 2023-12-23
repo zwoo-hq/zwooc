d3b659cc657fbf78996c3e8151745e3995b5862b
fabiankachlock <fabian.kachlock@icloud.com>
2023-12-24 00:26:41 +0100

    feat: animated task view

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/urfave/cli/v2"
 	"github.com/zwoo-hq/zwooc/pkg/config"
 	"github.com/zwoo-hq/zwooc/pkg/helper"
+	"github.com/zwoo-hq/zwooc/pkg/tasks"
 	"github.com/zwoo-hq/zwooc/pkg/ui"
 )
 
@@ -16,7 +18,46 @@ type (
 	}
 )
 
+func sleepTask(name string, dur time.Duration) tasks.Task {
+	return tasks.NewTask(name, func(cancel <-chan bool) error {
+		select {
+		case <-cancel:
+			return nil
+		case <-time.After(dur * time.Second):
+			return nil
+		}
+	})
+}
+
 func main() {
+	ui.CreateFullScreenView(config.TaskList{
+		Name: "test",
+		Steps: []config.ExecutionStep{
+			{
+				Name:        "pre",
+				RunParallel: false,
+				Tasks: []tasks.Task{
+					sleepTask("task 1", 2),
+					sleepTask("task 2", 3),
+					sleepTask("task 3", 4),
+				},
+			},
+			{
+				Name: "main",
+				Tasks: []tasks.Task{
+					sleepTask("dotnet build", 7),
+				},
+			},
+			{
+				Name: "post",
+				Tasks: []tasks.Task{
+					sleepTask("cleanup", 4),
+				},
+			},
+		},
+	})
+
+	return
 	path, err := helper.FindFile("zwoo.config.json")
 	if err != nil {
 		ui.HandleError(err)
