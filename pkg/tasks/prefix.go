9d4e7e83c9bf9478a48fbc5b62b06185495aefab
fabiankachlock <fabian.kachlock@icloud.com>
2023-12-28 15:57:54 +0100

    fix: command output prefixing

--- pkg/tasks/prefix.go
+++ pkg/tasks/prefix.go
@@ -1,12 +1,14 @@
 package tasks
 
 import (
+	"bytes"
 	"io"
 )
 
 type CommandPrefixer struct {
-	prefix []byte
-	dest   io.Writer
+	prefix    []byte
+	isMidLine bool
+	dest      io.Writer
 }
 
 var _ io.Writer = (*CommandPrefixer)(nil)
@@ -19,6 +21,32 @@ func NewPrefixer(prefix string, dest io.Writer) *CommandPrefixer {
 }
 
 func (r *CommandPrefixer) Write(p []byte) (n int, err error) {
-	n, err = r.dest.Write(append(r.prefix, p...))
-	return n - len(r.prefix), err
+	// split bytes into lines
+	lines := bytes.Split(p, []byte("\n"))
+	for i, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+
+		if i == 0 && r.isMidLine {
+			// do not prepend prefix
+			r.isMidLine = false
+		} else {
+			line = append(r.prefix, line...)
+			line = append(line, []byte("\n")...)
+		}
+
+		if i == len(lines)-1 {
+			// if the last line does not end with \n
+			// we need to remember that we are in the middle of a line
+			r.isMidLine = !bytes.HasSuffix(line, []byte("\n"))
+		}
+
+		// write the line
+		n, err = r.dest.Write(line)
+		if err != nil {
+			return n, err
+		}
+	}
+	return len(p), err
 }
