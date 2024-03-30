a23da7911a7ab8770d0efb13d80b11ca87986a2d
Fabian Kachlock <fabian.kachlock@icloud.com>
2024-03-30 14:12:08 +0100

    WIP: task tree runner

--- pkg/tasks/runner/tree_runner.go
+++ pkg/tasks/runner/tree_runner.go
@@ -1,11 +1,97 @@
 package runner
 
-import "github.com/zwoo-hq/zwooc/pkg/tasks"
+import (
+	"fmt"
+	"runtime"
+	"sync"
 
-type TaskTreeRunner struct {
-	root *tasks.TaskTreeNode
+	"github.com/zwoo-hq/zwooc/pkg/tasks"
+)
+
+type TreeStatusNode struct {
+	Name      string
+	Status    TaskStatus
+	PreNodes  []*TreeStatusNode
+	PostNodes []*TreeStatusNode
+	Parent    *TreeStatusNode
 }
 
-func NewTaskTreeRunner(root *tasks.TaskTreeNode) *TaskTreeRunner {
-	return &TaskTreeRunner{root: root}
+type TaskTreeRunner struct {
+	root           *tasks.TaskTreeNode
+	runningNodes   []*tasks.TaskTreeNode
+	status         *TreeStatusNode
+	updates        chan int
+	cancel         chan bool
+	cancelComplete chan error
+	mutex          sync.RWMutex
+	maxConcurrency int
+}
+
+func NewTaskTreeRunner(root *tasks.TaskTreeNode, maxConcurrency int) *TaskTreeRunner {
+	ticketAmount := maxConcurrency
+	if ticketAmount < 1 {
+		ticketAmount = runtime.NumCPU()
+	}
+	status := buildStatus(root)
+
+	return &TaskTreeRunner{
+		root:           root,
+		runningNodes:   []*tasks.TaskTreeNode{},
+		status:         status,
+		updates:        make(chan int, 1),
+		cancel:         make(chan bool),
+		cancelComplete: make(chan error),
+		maxConcurrency: ticketAmount,
+		mutex:          sync.RWMutex{},
+	}
+}
+
+func buildStatus(root *tasks.TaskTreeNode) *TreeStatusNode {
+	status := &TreeStatusNode{
+		Name:      root.Name,
+		Status:    StatusPending,
+		PreNodes:  []*TreeStatusNode{},
+		PostNodes: []*TreeStatusNode{},
+	}
+
+	for _, pre := range root.Pre {
+		status.PreNodes = append(status.PreNodes, buildStatus(pre))
+	}
+
+	for _, post := range root.Post {
+		status.PostNodes = append(status.PostNodes, buildStatus(post))
+	}
+
+	return status
+}
+
+func findStatus(status *TreeStatusNode, target *tasks.TaskTreeNode) *TreeStatusNode {
+	path := []string{target.Name}
+	for target.Parent != nil {
+		path = append([]string{target.Parent.Name}, path...)
+		target = target.Parent
+	}
+	fmt.Println(path)
+	current := status
+	for _, name := range path {
+		if current.Name == name {
+			return current
+		}
+
+		for _, pre := range current.PreNodes {
+			if pre.Name == name {
+				current = pre
+				continue
+			}
+		}
+
+		for _, post := range current.PostNodes {
+			if post.Name == name {
+				current = post
+				continue
+			}
+		}
+		break
+	}
+	return nil
 }
---
+++ pkg/tasks/runner/tree_runner_test.go
@@ -0,0 +1,123 @@
+package runner
+
+import (
+	"testing"
+
+	"github.com/zwoo-hq/zwooc/pkg/tasks"
+)
+
+func TestBuildStatus(t *testing.T) {
+	t.Run("TestbuildStatus", func(t *testing.T) {
+		tree := &tasks.TaskTreeNode{
+			Name: "root",
+			Pre: []*tasks.TaskTreeNode{
+				{
+					Name: "pre1",
+					Pre: []*tasks.TaskTreeNode{
+						{
+							Name: "pre1-1",
+						},
+					},
+				},
+			},
+			Post: []*tasks.TaskTreeNode{
+				{
+					Name: "post1",
+					Post: []*tasks.TaskTreeNode{
+						{
+							Name: "post1-1",
+						},
+					},
+				},
+			},
+		}
+
+		status := buildStatus(tree)
+
+		if status.Name != "root" {
+			t.Errorf("Expected root, got %s", status.Name)
+		}
+		if status.Status != StatusPending {
+			t.Errorf("Expected pending, got %d", status.Status)
+		}
+		if len(status.PreNodes) != 1 {
+			t.Errorf("Expected 1, got %d", len(status.PreNodes))
+		}
+		if len(status.PostNodes) != 1 {
+			t.Errorf("Expected 1, got %d", len(status.PostNodes))
+		}
+		if status.PreNodes[0].Name != "pre1" {
+			t.Errorf("Expected pre1, got %s", status.PreNodes[0].Name)
+		}
+		if status.PostNodes[0].Name != "post1" {
+			t.Errorf("Expected post1, got %s", status.PostNodes[0].Name)
+		}
+		if len(status.PreNodes[0].PreNodes) != 1 {
+			t.Errorf("Expected 1, got %d", len(status.PreNodes[0].PreNodes))
+		}
+		if len(status.PostNodes[0].PostNodes) != 1 {
+			t.Errorf("Expected 1, got %d", len(status.PostNodes[0].PostNodes))
+		}
+		if status.PreNodes[0].PreNodes[0].Name != "pre1-1" {
+			t.Errorf("Expected pre1-1, got %s", status.PreNodes[0].PreNodes[0].Name)
+		}
+		if status.PostNodes[0].PostNodes[0].Name != "post1-1" {
+			t.Errorf("Expected post1-1, got %s", status.PostNodes[0].PostNodes[0].Name)
+		}
+	})
+}
+
+func TestFindStatus(t *testing.T) {
+	t.Run("TestFindStatus", func(t *testing.T) {
+		tree := &tasks.TaskTreeNode{
+			Name: "root",
+			Pre: []*tasks.TaskTreeNode{
+				{
+					Name: "pre1",
+					Pre: []*tasks.TaskTreeNode{
+						{
+							Name: "pre1-1",
+						},
+					},
+				},
+			},
+			Post: []*tasks.TaskTreeNode{
+				{
+					Name: "post1",
+					Post: []*tasks.TaskTreeNode{
+						{
+							Name: "post1-1",
+						},
+					},
+				},
+			},
+		}
+
+		status := buildStatus(tree)
+
+		node := findStatus(status, tree)
+		if node.Name != "root" {
+			t.Errorf("Expected root, got %s", node.Name)
+		}
+
+		node = findStatus(status, tree.Pre[0])
+		if node.Name != "pre1" {
+			t.Errorf("Expected pre1, got %s", node.Name)
+		}
+
+		node = findStatus(status, tree.Post[0])
+		if node.Name != "post1" {
+			t.Errorf("Expected post1, got %s", node.Name)
+		}
+
+		node = findStatus(status, tree.Pre[0].Pre[0])
+		if node.Name != "pre1-1" {
+			t.Errorf("Expected pre1-1, got %s", node.Name)
+		}
+
+		node = findStatus(status, tree.Post[0].Post[0])
+		if node.Name != "post1-1" {
+			t.Errorf("Expected post1-1, got %s", node.Name)
+		}
+	})
+}
